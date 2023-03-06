ebd1d05eac09d3ef0cf0c185ec09f98c204b0340
Adam Smith <Smittysadam@gmail.com>
2023-03-06 11:13:31 -0600

    Initial Commit

---
+++ AdamsKeebs/AdamsKeebsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdamsKeebsApp.swift
+//  AdamsKeebs
+//
+//  Created by Adam Smith on 15/12/1401 AP.
+//
+
+import SwiftUI
+
+@main
+struct AdamsKeebsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AdamsKeebs/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AdamsKeebs
+//
+//  Created by Adam Smith on 15/12/1401 AP.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
