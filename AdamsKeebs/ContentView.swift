658804cef719f14b436d6ec3902cbaffa8ddf3be
Adam Smith <Smittysadam@gmail.com>
2023-03-09 12:19:15 -0600

    first commit

--- AdamsKeebs/ContentView.swift
+++ AdamsKeebs/ContentView.swift
@@ -6,21 +6,97 @@
 //
 
 import SwiftUI
+import URLImage
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+func extractImageURLs(from text: String) -> [String]? {
+    let detector = try! NSDataDetector(types: NSTextCheckingResult.CheckingType.link.rawValue)
+    let matches = detector.matches(in: text, options: [], range: NSRange(location: 0, length: text.utf16.count))
+    return matches.compactMap { match in
+        guard let range = Range(match.range, in: text) else { return nil }
+        let urlString = text[range]
+        let url = URL(string: String(urlString))
+        if let host = url?.host, host.contains("imgur.com") {
+            return url?.absoluteString
+        } else {
+            return nil
         }
-        .padding()
+    }
+}
+
+
+struct NewPostView: View {
+    @StateObject var viewModel = ViewModel()
+    
+    var body: some View {
+        NavigationView {
+            List(viewModel.post, id: \.self) { post in
+                NavigationLink(destination: PostDetailView(post: post)) {
+                    VStack {
+                        Text(post.title)
+                            .font(.headline)
+                        if let imageURLs = extractImageURLs(from: post.selftext) {
+                            ScrollView(.horizontal) {
+                                HStack {
+                                    ForEach(imageURLs, id: \.self) { url in
+                                        let _ = print(imageURLs)
+                                        AsyncImage(url: URL(string: url)) { image in
+                                            image
+                                                .resizable()
+                                                .aspectRatio(contentMode: .fit)
+                                        } placeholder: {
+                                            ProgressView()
+                                        }
+                                        .frame(height: 200)
+                                        
+                                    }
+                                    
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Mech Market")
+            .onAppear() {
+                viewModel.fetch()
+            }
+        }
+    }
+}
+
+struct PostDetailView: View {
+    let post: PostData
+    
+    var body: some View {
+        ScrollView(.vertical) {
+            VStack {
+                Text(post.title)
+                    .font(.headline)
+                if let imageURLs = extractImageURLs(from: post.selftext) {
+                    ScrollView(.horizontal) {
+                        HStack(spacing: 10) {
+                            ForEach(imageURLs, id: \.self) { url in
+                                AsyncImage(url: URL(string: url)) { image in
+                                    image
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                } placeholder: {
+                                    ProgressView()
+                                }
+                                .frame(height: 200)
+                            }
+                        }
+                    }
+                }
+                Text(post.selftext)
+                    .font(.headline)
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        NewPostView()
     }
 }
---
+++ AdamsKeebs/ViewModel.swift
@@ -0,0 +1,53 @@
+//
+//  ViewModel.swift
+//  AdamsKeebs
+//
+//  Created by Adam Smith on 16/12/1401 AP.
+//
+
+import Foundation
+import SwiftUI
+
+struct MechMarket: Hashable, Codable {
+    let kind: String?
+    let data: MechData
+}
+
+struct MechData: Hashable, Codable {
+    let children: [ChildData]
+}
+
+struct ChildData: Hashable, Codable {
+    let kind: String?
+    let data: PostData
+}
+
+struct PostData: Hashable, Codable {
+    let selftext: String
+    let title: String
+}
+
+class ViewModel: ObservableObject {
+    @Published var post: [PostData] = []
+    
+    func fetch() {
+        guard let url = URL(string: "https://www.reddit.com/r/mechmarket/new.json") else {
+            return
+        }
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data else {
+                return
+            }
+            do {
+                let postData = try JSONDecoder().decode(MechMarket.self, from: data)
+                let post = postData.data.children.map({ $0.data })
+                DispatchQueue.main.sync {
+                    self.post = post
+                }
+            } catch {
+                print(error)
+            }
+        }
+        task.resume()
+    }
+}
